0ed563056ce6e4b0fe19fe854a498cf865eba8b4
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-18 07:26:42 +0200

    edit comploader.jsx

--- src/components/CompLoader/CompLoader.jsx
+++ src/components/CompLoader/CompLoader.jsx
@@ -1,12 +1,7 @@
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './CompLoader.scss';
 
 const CompLoader = () => {
   return (
-    // <div className="compLoader flex_center">
-    //   <FontAwesomeIcon icon={faSpinner} spin />
-    // </div>
     <div className="loader_cont">
       <div className="loader">
         <span></span>
