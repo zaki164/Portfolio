fccf38719e7970029244bebc7727d3e685b57344
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-18 07:24:32 +0200

    layout.jsx file

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,10 +1,7 @@
-import { faRightLeft } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useLayoutEffect, useState } from "react";
-import { Header, Sidebar } from "../components"
 import Router from "../Router/Router";
-import "./Layout.scss";
 import { Navbar, Footer } from "../components";
+import "./Layout.scss";
 const Layout = () => {
   const [loading, setloading] = useState(true);
   const [light, setlight] = useState(false);
@@ -22,21 +19,20 @@ const Layout = () => {
       setloading(false);
     }, 1500)
   })
-  // localStorage.clear()
   return loading ?
-  <div className="loader_cont">
-    <div className="loader">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  </div> : (
-    <>
-      <Navbar lightMode={light} />
-      <Router />
-      <Footer />
-    </>
-  )
+    <div className="loader_cont">
+      <div className="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div> : (
+      <>
+        <Navbar lightMode={light} />
+        <Router />
+        <Footer />
+      </>
+    )
 }
 
 export default Layout
