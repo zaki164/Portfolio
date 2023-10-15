ca1af99a7164e79ed7aeffaf74f1590cb721e8f9
zaki164 <zakimohsen14@gmail.com>
2023-10-15 12:07:54 +0300

    update cv

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,21 +1,29 @@
-import { faHandPointRight } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Col, Container, Row } from 'react-bootstrap';
-import './About.scss';
+import { faHandPointRight } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Col, Container, Row } from "react-bootstrap";
+import "./About.scss";
 
-import AOS from 'aos';
+import AOS from "aos";
 AOS.init({
   once: true,
 });
 
 const About = () => {
   return (
-    <section className='About section_padding'>
-      <h2 className='main_heading'>About Me</h2>
+    <section className="About section_padding">
+      <h2 className="main_heading">About Me</h2>
       <Container>
         <article className="qual_cont" data-aos={"fade-up"}>
-          <p>Hi , I'm a Front-end web developer with <span>1</span>+ years of experience in designing and building responsive web apps.</p>
-          <p><span>2022</span><FontAwesomeIcon icon={faHandPointRight} /> Graduated with a very good grade , Bachelor in Computers and Control Systems Engineering, Mansoura University.</p>
+          <p>
+            Hi , I'm a Front-end web developer with <span>1</span>+ years of
+            experience in designing and building responsive web apps.
+          </p>
+          <p>
+            <span>2022</span>
+            <FontAwesomeIcon icon={faHandPointRight} /> Graduated with a very
+            good grade , Bachelor in Computers and Control Systems Engineering,
+            Mansoura University.
+          </p>
         </article>
         <article className="About_Info" data-aos={"fade-up"}>
           <Row>
@@ -25,7 +33,7 @@ const About = () => {
             </Col>
             <Col xs={12} md={6} className="col_info mb-3">
               <p>Age:</p>
-              <p className='fontalt mt-1'>24</p>
+              <p className="fontalt mt-1">24</p>
             </Col>
             <Col xs={12} md={6} className="col_info mb-3">
               <p>Country:</p>
@@ -33,25 +41,35 @@ const About = () => {
             </Col>
             <Col xs={12} md={6} className="col_info mb-3">
               <p>CV:</p>
-              <a href="https://drive.google.com/file/d/1fu8vJkY0-1rxVKy0DElN3w3K-TgkpAfs/view?usp=share_link" className='view custom_button' target="_blank" rel="noreferrer">View</a>
+              <a
+                href="https://drive.google.com/file/d/11szYx2YyYRYd7tTib5b8FitgaxVX_6zi/view?usp=sharing"
+                className="view custom_button"
+                target="_blank"
+                rel="noreferrer"
+              >
+                View
+              </a>
             </Col>
             <Col xs={12} md={6} className="col_info mb-0">
               <p>Phone:</p>
-              <p className='fontalt mt-1'>01010386649</p>
+              <p className="fontalt mt-1">01010386649</p>
             </Col>
             <Col xs={12} md={6} className="col_info mb-0">
               <p>Gmail:</p>
-              <p className='gmail'>zakimohsen14@gmail.com</p>
+              <p className="gmail">zakimohsen14@gmail.com</p>
             </Col>
           </Row>
         </article>
         <article className="About_passion" data-aos={"fade-up"}>
           <p>Coding is my passion , I love to learn something new everyday.</p>
-          <p><FontAwesomeIcon icon={faHandPointRight} /> And Finally , My favourite Sport is Football.</p>
+          <p>
+            <FontAwesomeIcon icon={faHandPointRight} /> And Finally , My
+            favourite Sport is Football.
+          </p>
         </article>
       </Container>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
