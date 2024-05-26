f097c970b01689ff1e8d7ea44bf05317414db405
zaki164 <zakimohsen14@gmail.com>
2024-05-26 09:54:29 +0300

    reduce images size

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,24 +1,24 @@
-import profile from "../assets/profile.jpg";
-import contact from "../assets/contact_img.png";
-import contact2 from "../assets/contact.png";
-import project1 from "../assets/Projects/Courselist App.png";
-import project2 from "../assets/Projects/Dracarys.png";
-import project3 from "../assets/Projects/Galaxy.png";
-import project4 from "../assets/Projects/Gericht.png";
-import project5 from "../assets/Projects/Kasper.png";
-import project6 from "../assets/Projects/leon.png";
-import project7 from "../assets/Projects/Psycho.png";
-import project8 from "../assets/Projects/Sahem.png";
-import project9 from "../assets/Projects/Special Design.png";
-import project10 from "../assets/Projects/Todolist App.png";
-import project11 from "../assets/Projects/Weather App.png";
-import project12 from "../assets/Projects/Atsky.png";
-import project13 from "../assets/Projects/Nike.png";
-import project14 from "../assets/Projects/Scene Cinemas.png";
-import project15 from "../assets/Projects/Project NXT.png";
-import project16 from "../assets/Projects/itad.png";
-import project17 from "../assets/Projects/pickappo.png";
-import project18 from "../assets/Projects/mandobk.png";
+import profile from "../assets/profile.webp";
+import contact from "../assets/contact_img.webp";
+import contact2 from "../assets/contact.webp";
+import project1 from "../assets/Projects/Courselist App.webp";
+import project2 from "../assets/Projects/Dracarys.webp";
+import project3 from "../assets/Projects/Galaxy.webp";
+import project4 from "../assets/Projects/Gericht.webp";
+import project5 from "../assets/Projects/Kasper.webp";
+import project6 from "../assets/Projects/leon.webp";
+import project7 from "../assets/Projects/Psycho.webp";
+import project8 from "../assets/Projects/Sahem.webp";
+import project9 from "../assets/Projects/Special Design.webp";
+import project10 from "../assets/Projects/Todolist App.webp";
+import project11 from "../assets/Projects/Weather App.webp";
+import project12 from "../assets/Projects/Atsky.webp";
+import project13 from "../assets/Projects/Nike.webp";
+import project14 from "../assets/Projects/Scene Cinemas.webp";
+import project15 from "../assets/Projects/Project NXT.webp";
+import project16 from "../assets/Projects/itad.webp";
+import project17 from "../assets/Projects/pickappo.webp";
+import project18 from "../assets/Projects/mandobk.webp";
 
 const images = {
   profile,
